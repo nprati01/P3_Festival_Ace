0a1ec8709a675278c94f707c6e7712369bf932b9
nprati01 <nicole.e.prati@gmail.com>
2023-04-18 18:04:42 -0400

    add festival to my festival functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('festivals/', views.festivals_index, name='festivals_index'),
-
+    path('festivals/<int:festival_id>/', views.festivals_detail, name='detail'),
+    path('add_festival/<int:festival_id>', views.add_festival, name='add_festival'),
     path('myfestivals/', views.my_festivals_index, name='my_festivals_index'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Festival, MyFestivalPlanning
 
 
@@ -9,7 +10,25 @@ def home(request):
 
 def festivals_index(request):
     festivals = Festival.objects.all()
-    return render(request, 'festivals_index.html', {'festivals': festivals})
+    return render(request, 'festivals/index.html', {'festivals': festivals})
+
+def festivals_detail(request, festival_id):
+    festival = Festival.objects.get(id=festival_id)
+    return render(request, 'festivals/detail.html', { 'festival': festival})
+
+def add_festival(request, festival_id):
+    festival = get_object_or_404(Festival, id=festival_id)
+    print(festival)
+    in_my_festivals = MyFestivalPlanning.objects.filter(user=request.user, festival=festival)
+    print(in_my_festivals)
+    if in_my_festivals.exists():
+            return redirect('detail', festival_id=festival_id)
+    add_to_my_festivals = MyFestivalPlanning(user=request.user, festival=festival)
+    print(add_to_my_festivals)
+    add_to_my_festivals.save()
+    return redirect('detail', festival_id=festival_id)
+
+
 
 
 def my_festivals_index(request):
