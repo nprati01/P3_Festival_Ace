e4f0d3c0485b5d6fed2a14ea2c2c1789ad7850b7
nprati01 <nicole.e.prati@gmail.com>
2023-04-19 11:47:29 -0400

    add MyFestival model to admins

--- festivalace/settings.py
+++ festivalace/settings.py
@@ -123,3 +123,5 @@ LOGIN_REDIRECT_URL ='/festivals/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Festival
+from .models import Festival, MyFestivalPlanning
 # Register your models here.
 admin.site.register(Festival)
+admin.site.register(MyFestivalPlanning)
